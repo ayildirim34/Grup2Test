a929b74296503d30abbb4d3ae3969694b22ac4a1
muhammetkarakaya <karakayaatc@gmail.com>
2022-04-11 22:38:59 +0200

    . gönderi

--- src/test/java/Grup2Paket/Grup2Test.java
+++ src/test/java/Grup2Paket/Grup2Test.java
@@ -2,6 +2,8 @@ package Grup2Paket;
 
 public class Grup2Test {
     public static void main(String[] args) {
+
         System.out.println("basladik bakalim");
+        System.out.println("2. Adim");
     }
 }
