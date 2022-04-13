a3f37b25ab2e0ddc14628c4123c5a4c59b2e8fe8
zeynepozer1 <zozer7510@gmail.com>
2022-04-13 19:07:29 +0300

    mesajım

--- src/test/java/Grup2Paket/Grup2Test.java
+++ src/test/java/Grup2Paket/Grup2Test.java
@@ -7,5 +7,7 @@ public class Grup2Test {
         System.out.println("2. Adim");
         System.out.println("3.adim");
         System.out.println( "4. adım.Zeynep");
+        System.out.println( "Deneme");
     }
 }
+
